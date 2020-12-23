96dc15740bee26db81cc514a7f67703efab208bd
akcgjc007 <akcgjc007@gmail.com>
2020-12-23 18:57:32 +0530

    Removed recent section

---
+++ js/myscripts.js
@@ -0,0 +1,30 @@
+function CopyToClipboard(id) {
+    var r = document.createRange();
+    r.selectNode(document.getElementById(id));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+}
+
+//Get the button
+var mybutton = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+const breakPoint = 900;
+
+function scrollFunction() {
+    if (document.body.scrollTop > breakPoint || document.documentElement.scrollTop > breakPoint) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
